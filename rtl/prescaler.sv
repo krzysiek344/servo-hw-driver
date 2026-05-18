a6846c514cc25680df372878c2b445e299373aa7
Olliweir <oliwierkrupaa@gmail.com>
2026-05-18 17:35:56 +0200

    fix(delete delay)

--- rtl/prescaler.sv
+++ rtl/prescaler.sv
@@ -6,35 +6,38 @@ module prescaler #(
 )(
     input logic clk,
     input logic rst_n,
+    output logic step_tick,
     input logic enable,
-    input logic [SCALE_WIDTH-1:0] scale_val
+    input logic [31:0] scale_val
 );
-    logic [SCALE_WIDTH-1:0] counter, counter_nxt;
-    logic step_tick_nxt;
+    logic [31:0] counter, counter_nxt;
 
     always_ff @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
-            counter <= '0;
-            step_tick <= 1'b0;
+            counter <= 32'd0;
         end else begin
             counter <= counter_nxt;
-            step_tick <= step_tick_nxt;
         end
     end
 
     always_comb begin
         counter_nxt = counter;
-        step_tick_nxt = 1'b0;
-
         if (enable) begin
-            if (counter >= scale_val - 1) begin
-                counter_nxt = '0;
-                step_tick_nxt = 1'b1;
+            if (counter >= (scale_val - 32'd1)) begin
+                counter_nxt = 32'd0;
             end else begin
-                counter_nxt = counter + 1;
+                counter_nxt = counter + 32'd1;
             end
         end else begin
-            counter_nxt = '0;
+            counter_nxt = 32'd0;
         end
     end
+
+    always_comb begin
+        step_tick = 1'b0;
+        if (enable && (counter >= (scale_val - 32'd1))) begin
+            step_tick = 1'b1;
+        end
+    end
+
 endmodule
