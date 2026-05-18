37b29a78b99c12ab952ffd7493841c32a69be277
Olliweir <oliwierkrupaa@gmail.com>
2026-05-18 17:38:50 +0200

    refactorization

--- rtl/step_counter.sv
+++ rtl/step_counter.sv
@@ -8,14 +8,14 @@ module step_counter #(
     input  logic step_tick,
     input  logic dir,
     input  logic set_zero, // z poziomu FSM mozesz wyzerowac licznik przez ta zmienna 
-    output logic [POS_RANGE-1:0] current_pos
+    output logic signed [31:0] current_pos
 );
 
-    logic [POS_RANGE-1:0] current_pos_nxt;
+    logic signed [31:0] current_pos_nxt;
 
     always_ff @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
-            current_pos <= '0;
+            current_pos <= 32'sd0;
         end else begin
             current_pos <= current_pos_nxt;
         end
@@ -25,12 +25,12 @@ module step_counter #(
         current_pos_nxt = current_pos;
 
         if (set_zero) begin
-            current_pos_nxt = '0;
+            current_pos_nxt = 32'sd0;
         end else if (step_tick) begin
             if (dir) begin
-                current_pos_nxt = current_pos + 1;
+                current_pos_nxt = current_pos + 32'sd1;
             end else begin
-                current_pos_nxt = current_pos - 1;
+                current_pos_nxt = current_pos - 32'sd1;
             end
         end
     end
