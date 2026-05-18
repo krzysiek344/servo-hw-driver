d53dc6d8d74daa6ece8e6a19c702f6e1377c65d4
Olliweir <oliwierkrupaa@gmail.com>
2026-05-18 23:25:59 +0200

    refactor(prescaler): drop int param type

--- rtl/prescaler.sv
+++ rtl/prescaler.sv
@@ -2,19 +2,20 @@
 `timescale 1ns/1ps
 
 module prescaler #(
-    parameter int SCALE_WIDTH = 32
+    parameter SCALE_WIDTH = 32
 )(
     input logic clk,
     input logic rst_n,
     output logic step_tick,
     input logic enable,
-    input logic [31:0] scale_val
+    input logic [SCALE_WIDTH-1:0] scale_val
+   
 );
-    logic [31:0] counter, counter_nxt;
+    logic [SCALE_WIDTH-1:0] counter, counter_nxt;
 
     always_ff @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
-            counter <= 32'd0;
+            counter <= '0;
         end else begin
             counter <= counter_nxt;
         end
@@ -23,19 +24,19 @@ module prescaler #(
     always_comb begin
         counter_nxt = counter;
         if (enable) begin
-            if (counter >= (scale_val - 32'd1)) begin
-                counter_nxt = 32'd0;
+            if (counter >= (scale_val - 1)) begin
+                counter_nxt = '0;
             end else begin
-                counter_nxt = counter + 32'd1;
+                counter_nxt = counter + 1;
             end
         end else begin
-            counter_nxt = 32'd0;
+            counter_nxt = '0;
         end
     end
 
     always_comb begin
         step_tick = 1'b0;
-        if (enable && (counter >= (scale_val - 32'd1))) begin
+        if (enable && (counter >= (scale_val - 1))) begin
             step_tick = 1'b1;
         end
     end
