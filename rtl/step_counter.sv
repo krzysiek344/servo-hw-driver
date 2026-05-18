7d1d37bbd3736d86a8ffe37802920a56baa2f448
Olliweir <oliwierkrupaa@gmail.com>
2026-05-18 23:27:34 +0200

    refactor(step_counter): drop trailing comma and int

--- rtl/step_counter.sv
+++ rtl/step_counter.sv
@@ -1,21 +1,22 @@
 `timescale 1ns / 1ps
 
 module step_counter #(
-    parameter int POS_RANGE = 32
+    parameter POS_RANGE = 32
 )(
     input  logic clk,
     input  logic rst_n,
+    output logic [POS_RANGE-1:0] current_pos,
     input  logic step_tick,
     input  logic dir,
-    input  logic set_zero, // z poziomu FSM mozesz wyzerowac licznik przez ta zmienna 
-    output logic signed [31:0] current_pos
+    input  logic set_zero  // z poziomu FSM mozesz wyzerowac licznik przez ta zmienna 
+    
 );
 
-    logic signed [31:0] current_pos_nxt;
+    logic [POS_RANGE-1:0] current_pos_nxt;
 
     always_ff @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
-            current_pos <= 32'sd0;
+            current_pos <= '0;
         end else begin
             current_pos <= current_pos_nxt;
         end
@@ -25,12 +26,12 @@ module step_counter #(
         current_pos_nxt = current_pos;
 
         if (set_zero) begin
-            current_pos_nxt = 32'sd0;
+            current_pos_nxt = '0;
         end else if (step_tick) begin
             if (dir) begin
-                current_pos_nxt = current_pos + 32'sd1;
+                current_pos_nxt = current_pos + 1;
             end else begin
-                current_pos_nxt = current_pos - 32'sd1;
+                current_pos_nxt = current_pos - 1;
             end
         end
     end
